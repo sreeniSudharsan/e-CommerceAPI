181e55f078a3080806921e18e08ef8db11b15179
Sreenivasan <sreenivasansudharsan49@gmail.com>
2023-01-25 10:38:07 +0530

    Fixed routing bug in order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const {authenticateUser,authorizePermissions} = require('../middleware/authentication');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
 
 const {
-    createOrder,
-    getAllOrders,
-    getSingleOrder,
-    getCurrentUserOrders,
-    updateOrders
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  createOrder,
+  updateOrder,
 } = require('../controller/orderController');
 
-router.route('/').post(authenticateUser, createOrder).get(authenticateUser, authorizePermissions('admin'), getAllOrders);
+router
+  .route('/')
+  .post(authenticateUser, createOrder)
+  .get(authenticateUser, authorizePermissions('admin'), getAllOrders);
 
-router.route('/showAllMyOrders').get(authenticateUser, getCurrentUserOrders)
+router.route('/showAllMyOrders').get(authenticateUser, getCurrentUserOrders);
 
-router.route('/:id').get(authenticateUser, getSingleOrder)
-                    .patch(authenticateUser, updateOrders)
-
+router
+  .route('/:id')
+  .get(authenticateUser, getSingleOrder)
+  .patch(authenticateUser, updateOrder);
 
 module.exports = router;
-
