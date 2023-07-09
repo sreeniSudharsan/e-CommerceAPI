e9291b97666f044eb7999f51d6736e85b01017c9
Sreenivasan R S <sreenivasansudharsan49@gmail.com>
2023-07-09 23:19:30 +0530

    Add FileImageUploadController

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use(express.static('./public/upload1s'));
+app.use(express.static('./public/upload/vader.jpg'));
 app.use(fileUpload());
 
 app.use('/api/v1/auth', authRouter);
--- controller/productController.js
+++ controller/productController.js
@@ -52,9 +52,31 @@ if(!product){
 res.status(StatusCodes.CREATED).json({product})
 }
 
-const uploadImage = async(req, res) => {
-    res.send('Create Product')
-}
+const uploadImage = async (req, res) => {
+    if (!req.files) {
+      throw new CustomError.BadRequestError('No File Uploaded');
+    }
+    const productImage = req.files.image;
+  
+    if (!productImage.mimetype.startsWith('image')) {
+      throw new CustomError.BadRequestError('Please Upload Image');
+    }
+  
+    const maxSize = 1024 * 1024;
+  
+    if (productImage.size > maxSize) {
+      throw new CustomError.BadRequestError(
+        'Please upload image smaller than 1MB'
+      );
+    }
+  
+    const imagePath = path.join(
+      __dirname,
+      '../public/uploads/' + `${productImage.name}`
+    );
+    await productImage.mv(imagePath);
+    res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
+  };
 
 module.exports = {createProduct, 
                 getAllProduct, 
